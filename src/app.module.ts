2680f1eee6944e8885de2619481ac02069ec5538
Jay Min <jay@Mac-1134.lan>
2026-02-12 19:04:20 -0500

    chore: Change Folder name typeorm -> config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import typeorm from './typeorm/typeorm';
+import typeorm from './config/typeorm.config';
 import { DataBaseModule } from './database/database.module';
 
 @Module({
